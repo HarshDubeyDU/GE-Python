6d8c7dcddaaaf81491345963f2b6a42339928411
Harsh Dubey <87745474+HarshDubeyDU@users.noreply.github.com>
2023-10-06 11:50:59 +0530

    Update Practical3.py to have clear code4

--- Practical3.py
+++ Practical3.py
@@ -6,11 +6,12 @@ counter = 1
 for i in range(1, n+1, 2):
     print(spaces + ("*" * i))
     spaces = " " * (int(n/2) - counter)
-    counter+=1
+    counter += 1
 
-#Reverse pyramid
+# Reverse pyramid
 counter = 1
 for i in range(n-2, 0, -2):
     spaces = " " * counter
     print(spaces + ("*" * i))
-    counter+=1
+    counter += 1
+    
