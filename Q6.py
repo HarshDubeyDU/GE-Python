dc2300fe26b7902adf325cf2a2158165725f812a
Harsh Dubey <87745474+HarshDubeyDU@users.noreply.github.com>
2023-10-11 10:14:59 +0530

    Practical 6

---
+++ Q6.py
@@ -0,0 +1,14 @@
+strOne = input("Enter the first String : ")
+strTwo = input("Enter the second String : ")
+
+n = int(input("Length of the string to be swapped : "))
+
+if n <= min(len(strOne), len(strTwo)):
+    temp = strOne[0: n]
+    strOne = strOne.replace(temp, strTwo[0: n])
+    strTwo = strTwo.replace(strTwo[0: n], temp)
+else:
+    print("Wrong Input!!")
+
+print("First String :", strOne)
+print("Second String :", strTwo)
